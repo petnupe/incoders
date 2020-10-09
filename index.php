655dbdc3b34640fa0d46f74d3fc1304e60a56dc8
PETERSON\Peterson <peterson@tecbiz.com.br>
2020-10-09 14:50:49 -0300

    arquivos iniciais 2

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ class MailReader {
             if ($nOfMessages > 0) {
                 for ($message = 1; $message <= $nOfMessages; $message++) {
                     $body = imap_fetchbody($this->mailBox, $message, 1);
+                    $this->pegaDados($body);
                 }
             }
             imap_close($this->mailBox);
@@ -37,8 +38,40 @@ class MailReader {
             print_r($errors);
             echo "</pre>";
     }
+
+    private function pegaDados($texto) {
+        $ini = strpos($texto, "Nome");
+        $fim = stripos($texto, "Att.");
+
+        $conteudo = substr($texto, $ini, ($fim-$ini));
+        die($conteudo);
+        $parse = str_replace(':', '=', $conteudo);
+        $final = parse_ini_string($parse);
+
+        foreach ($final as $key => $value) {
+            echo $key .' => ' .$value.'<br >';
+        }
+
+        echo "<br /><br />";
+    }
 }
 
-//ini_set('display_errors', 1); error_reporting(E_ALL);
+ini_set('display_errors', 1); error_reporting(E_ALL);
 $read = new MailReader();
 $read->Read();
+
+/*
+$texto = 'Date: sex., 9 de out. de 2020 às 13:17
+Subject: Teste
+To: <rrdasoliveiras@gmail.com>
+Bom dia,
+Segue meus dados de contato e informações para pagamento
+Nome: Guarida Imóveis
+Endereço: Protásio alves, 1309
+Valor: R$1.300,50
+Vencimento:12/19
+Att.
+Peterson.png
+Peterson Nunes';
+
+*/
